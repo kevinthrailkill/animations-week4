3e71ba33e34abd396bfe035c8a43ae0c80158192
Utkarsh Sengar <utkarsh2012@gmail.com>
2017-04-19 19:40:00 -0700

    pan tray

--- Lab4-Animations/Lab4-Animations/ViewController.swift
+++ Lab4-Animations/Lab4-Animations/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var trayOriginalCenter: CGPoint!
+    
+    @IBOutlet weak var trayView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func panAction(_ panGestureRecognizer: UIPanGestureRecognizer) {
+        // Absolute (x,y) coordinates in parent view (parentView should be
+        // the parent view of the tray)
+        let point = panGestureRecognizer.location(in: self.view)
+        
+        if panGestureRecognizer.state == .began {
+            print("Gesture began at: \(point)")
+            trayOriginalCenter = trayView.center
+        } else if panGestureRecognizer.state == .changed {
+            let translation = panGestureRecognizer.translation(in: trayView)
+            print("Gesture changed at: \(point)")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if panGestureRecognizer.state == .ended {
+            print("Gesture ended at: \(point)")
+        }
+    }
 
 }
 
