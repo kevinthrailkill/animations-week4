767f3abecaac6270b6f7af471983131fafa575e6
Utkarsh Sengar <utkarsh2012@gmail.com>
2017-04-19 20:15:38 -0700

    pan gesture and drop faces

--- Lab4-Animations/Lab4-Animations/ViewController.swift
+++ Lab4-Animations/Lab4-Animations/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     var trayOriginalCenter: CGPoint!
     var trayCenterWhenOpen: CGPoint!
     var trayCenterWhenClosed: CGPoint!
+    var newlyCreatedFace: UIImageView!
     
     @IBOutlet weak var trayView: UIView!
     
@@ -64,5 +65,54 @@ class ViewController: UIViewController {
         }
     }
 
+    
+    @IBAction func smileyPanGesture(_ panGestureRecognizer: UIPanGestureRecognizer) {
+        let point = panGestureRecognizer.location(in: self.view)
+        
+        if panGestureRecognizer.state == .began {
+            print("Gesture began at: \(point)")
+            // Gesture recognizers know the view they are attached to
+            let imageView = panGestureRecognizer.view as! UIImageView
+            
+            // Create a new image view that has the same image as the one currently panning
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            
+            // Add the new face to the tray's parent view.
+            view.addSubview(newlyCreatedFace)
+            
+            // Initialize the position of the new face.
+            newlyCreatedFace.center = imageView.center
+            
+            // Since the original face is in the tray, but the new face is in the
+            // main view, you have to offset the coordinates
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            
+            // Here we use the method didPan(sender:), which we defined in the previous step, as the action.
+            let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didPan(sender:)))
+            
+            // Attach it to a view of your choice. If it's a UIImageView, remember to enable user interaction
+            newlyCreatedFace.isUserInteractionEnabled = true
+            newlyCreatedFace.addGestureRecognizer(panGestureRecognizer)
+            
+        } else if panGestureRecognizer.state == .changed {
+            print("Gesture changed at: \(point)")
+            newlyCreatedFace.center = point
+            
+        } else if panGestureRecognizer.state == .ended {
+            print("Gesture ended at: \(point)")
+        }
+    }
+    
+    func didPan(sender: UIPanGestureRecognizer) {        
+        if sender.state == .began {
+            print("Gesture began")
+            sender.view?.transform = CGAffineTransform(scaleX: 2, y: 2)
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+        } else if sender.state == .ended {
+            print("Gesture ended")
+            sender.view?.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }
+    }
 }
 
